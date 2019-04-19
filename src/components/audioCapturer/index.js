47ff98163de17c23c6e4eafbb8c878c510d5a26a
Alexander Zhukov <alexander.zhukov@native-instruments.de>
2019-04-19 20:47:00 +0200

    extract audio capturer

---
+++ src/components/audioCapturer/index.js
@@ -0,0 +1,43 @@
+import {html, render} from '@modulor-js/html';
+
+export default ({mediaRecorder}) => {
+  return range => {
+    var chunks = [];
+
+    mediaRecorder.addEventListener('dataavailable', e => {
+      console.log(e.data);
+      chunks.push(e.data);
+    });
+
+    mediaRecorder.addEventListener('stop', e => {
+      console.log('recorder stopped');
+
+      var clipName = prompt('Enter a name for your sound clip');
+
+      var clipContainer = document.createElement('article');
+      var clipLabel = document.createElement('p');
+      var audio = document.createElement('audio');
+      var deleteButton = document.createElement('button');
+
+      clipContainer.classList.add('clip');
+      audio.setAttribute('controls', '');
+      deleteButton.innerHTML = 'Delete';
+      clipLabel.innerHTML = clipName;
+
+      clipContainer.appendChild(audio);
+      clipContainer.appendChild(clipLabel);
+      clipContainer.appendChild(deleteButton);
+
+      var blob = new Blob(chunks, {type: 'audio/ogg; codecs=opus'});
+      chunks = [];
+      var audioURL = window.URL.createObjectURL(blob);
+      audio.src = audioURL;
+
+      render(clipContainer, range);
+      deleteButton.onclick = function(e) {
+        var evtTgt = e.target;
+        evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+      };
+    });
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import 'babel-polyfill';
 import {html, render} from '@modulor-js/html';
 
+import audioCapturer from './components/audioCapturer';
+
+
 (async () => {
   if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
     console.log('getUserMedia not supported on your browser!');
@@ -27,72 +30,9 @@ import {html, render} from '@modulor-js/html';
       <button onclick=${start}>start</button>
       <button onclick=${stop}>stop</button>
       <div>
-        <${({mediaRecorder}) => {
-          return range => {
-            var chunks = [];
-
-            mediaRecorder.addEventListener('dataavailable', (e) => {
-              console.log(e.data);
-              chunks.push(e.data);
-            });
-
-            mediaRecorder.addEventListener('stop', (e) => {
-              console.log('recorder stopped');
-
-              var clipName = prompt('Enter a name for your sound clip');
-
-              var clipContainer = document.createElement('article');
-              var clipLabel = document.createElement('p');
-              var audio = document.createElement('audio');
-              var deleteButton = document.createElement('button');
-
-              clipContainer.classList.add('clip');
-              audio.setAttribute('controls', '');
-              deleteButton.innerHTML = 'Delete';
-              clipLabel.innerHTML = clipName;
-
-              clipContainer.appendChild(audio);
-              clipContainer.appendChild(clipLabel);
-              clipContainer.appendChild(deleteButton);
-
-              var blob = new Blob(chunks, {type: 'audio/ogg; codecs=opus'});
-              chunks = [];
-              var audioURL = window.URL.createObjectURL(blob);
-              audio.src = audioURL;
-
-              render(clipContainer, range);
-              deleteButton.onclick = function(e) {
-                var evtTgt = e.target;
-                evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-              };
-            });
-          };
-        }} mediaRecorder=${mediaRecorder}>
+        <${audioCapturer} mediaRecorder=${mediaRecorder} />
       </div>
-      </${''}>
     `,
     document.querySelector('#app'),
   );
 })();
-
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  console.log('getUserMedia supported.');
-  navigator.mediaDevices
-    .getUserMedia(
-      // constraints - only audio needed for this app
-      {
-        audio: true,
-      },
-    )
-
-    // Success callback
-    .then(function(stream) {
-      console.log(stream);
-    })
-
-    // Error callback
-    .catch(function(err) {
-      console.log('The following getUserMedia error occured: ' + err);
-    });
-} else {
-}
