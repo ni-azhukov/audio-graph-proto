f30ce96ceac536e40fe220148c0b70673c287aa7
Alexander Zhukov <alexander.zhukov@native-instruments.de>
2019-04-20 21:58:57 +0200

    add sources

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {html, render} from '@modulor-js/html';
 import audioCapturer from './components/audioCapturer';
 import audioVisualiser from './components/audioVisualiser';
 
-
 (async () => {
   if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
     console.log('getUserMedia not supported on your browser!');
@@ -12,7 +11,6 @@ import audioVisualiser from './components/audioVisualiser';
   }
 
   const stream = await navigator.mediaDevices.getUserMedia({audio: true});
-
 
   var mediaRecorder = new MediaRecorder(stream);
 
@@ -37,6 +35,27 @@ import audioVisualiser from './components/audioVisualiser';
       </div>
       <div>
         <${audioCapturer} mediaRecorder=${mediaRecorder} />
+      </div>
+      <div>
+        ${(navigator.mediaDevices && navigator.mediaDevices.enumerateDevices)
+          ? navigator.mediaDevices.enumerateDevices()
+            .then(devices => {
+              return devices.map(
+                device =>
+                  html`
+                    <div>
+                      ${device.kind}: ${device.label} id = ${device.deviceId}
+                    </div>
+                  `,
+              );
+            })
+            .catch(function(err) {
+              return html`
+                ${err.name + ': ' + err.message}
+              `
+            })
+          : `enumerateDevices() not supported.`
+        }
       </div>
     `,
     document.querySelector('#app'),
